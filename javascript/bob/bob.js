0750a3e1363461af4cca61702a9a7213248d521c
Patrick Eriksson <paleite@users.noreply.github.com>
2022-03-13 00:29:25 +0100

    Progress

---
+++ javascript/bob/bob.js
@@ -0,0 +1,27 @@
+const NOT_SCREAMING = "NOT_SCREAMING";
+const SCREAMING = "SCREAMING";
+const NOT_ASKING = "NOT_ASKING";
+const ASKING = "ASKING";
+
+const response = {
+  [NOT_SCREAMING]: { [NOT_ASKING]: "Whatever.", [ASKING]: "Sure." },
+  [SCREAMING]: {
+    [NOT_ASKING]: "Whoa, chill out!",
+    [ASKING]: "Calm down, I know what I'm doing!",
+  },
+};
+
+const hey = (input) => {
+  const request = input.trim();
+
+  const isSilent = request === "";
+  const isQuestion = request.slice(-1) === "?" ? ASKING : NOT_ASKING;
+  const isScreaming =
+    request === request.toUpperCase() && request != request.toLowerCase()
+      ? SCREAMING
+      : NOT_SCREAMING;
+
+  return !isSilent ? response[isScreaming][isQuestion] : "Fine. Be that way!";
+};
+
+export { hey };
