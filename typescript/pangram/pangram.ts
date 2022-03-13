fb703e9daf17c64f048ae073ad512a872ec295be
Patrick Eriksson <paleite@users.noreply.github.com>
2022-03-13 01:30:40 +0100

    Progress

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,20 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+const includesLetter =
+  (arr: string[]) =>
+  (letter: string): boolean =>
+    arr.includes(letter);
+
+const getLetters = (input: string): string[] =>
+  input.replace(/\W+/giu, "").toLowerCase().split("");
+
+const alphabetsEvery = (letterIsIncluded: (arg0: string) => boolean): boolean =>
+  alphabet.every(letterIsIncluded);
+
+const isPangram = (input: string): boolean => {
+  const letterIsIncludedInInput = includesLetter(getLetters(input));
+
+  return alphabetsEvery(letterIsIncludedInInput);
+};
+
+export { isPangram };
