446c1dbe8708ce0e3066cb51b783fd077135efdb
Patrick Eriksson <paleite@users.noreply.github.com>
2022-04-04 23:03:48 +0200

    Progress

--- javascript/elyses-enchantments/enchantments.js
+++ javascript/elyses-enchantments/enchantments.js
@@ -9,7 +9,7 @@
  * @returns {number} the card
  */
 export function getItem(cards, position) {
-  throw new Error("Implement the getItem function")
+  return cards.at(position)
 }
 
 /**
@@ -22,7 +22,9 @@ export function getItem(cards, position) {
  * @returns {number[]} the cards with the change applied
  */
 export function setItem(cards, position, replacementCard) {
-  throw new Error("Implement the setItem function")
+  cards.splice(position, 1, replacementCard)
+
+  return cards
 }
 
 /**
@@ -34,7 +36,9 @@ export function setItem(cards, position, replacementCard) {
  * @returns {number[]} the cards with the newCard applied
  */
 export function insertItemAtTop(cards, newCard) {
-  throw new Error("Implement the insertItemAtTop function")
+  cards.push(newCard)
+
+  return cards
 }
 
 /**
@@ -46,7 +50,9 @@ export function insertItemAtTop(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItem(cards, position) {
-  throw new Error("Implement the removeItem function")
+  cards.splice(position, 1)
+
+  return cards
 }
 
 /**
@@ -57,7 +63,9 @@ export function removeItem(cards, position) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemFromTop(cards) {
-  throw new Error("Implement the removeItemFromTop function")
+  cards.pop()
+
+  return cards
 }
 
 /**
@@ -69,7 +77,9 @@ export function removeItemFromTop(cards) {
  * @returns {number[]} the cards including the new card
  */
 export function insertItemAtBottom(cards, newCard) {
-  throw new Error("Implement the insertItemAtBottom function")
+  cards.unshift(newCard)
+
+  return cards
 }
 
 /**
@@ -80,7 +90,9 @@ export function insertItemAtBottom(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemAtBottom(cards) {
-  throw new Error("Implement the removeItemAtBottom function")
+  cards.shift()
+
+  return cards
 }
 
 /**
@@ -92,5 +104,5 @@ export function removeItemAtBottom(cards) {
  * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
  */
 export function checkSizeOfStack(cards, stackSize) {
-  throw new Error("Implement the checkSizeOfStack function")
+  return cards.length === stackSize
 }
--- javascript/elyses-enchantments/enchantments.spec.js
+++ javascript/elyses-enchantments/enchantments.spec.js
@@ -9,24 +9,24 @@ import {
   removeItemFromTop,
   removeItemAtBottom,
   checkSizeOfStack,
-} from "./enchantments"
+} from './enchantments'
 
-describe("pick a card", () => {
-  test("get the first card", () => {
+describe('pick a card', () => {
+  test('get the first card', () => {
     const stack = [1, 2, 3]
     const expected = 1
 
     expect(getItem(stack, 0)).toBe(expected)
   })
 
-  test("get the middle card", () => {
+  test('get the middle card', () => {
     const stack = [4, 5, 6]
     const expected = 5
 
     expect(getItem(stack, 1)).toBe(expected)
   })
 
-  test("get the last card", () => {
+  test('get the last card', () => {
     const stack = [9, 8, 7]
     const expected = 7
 
@@ -34,8 +34,8 @@ describe("pick a card", () => {
   })
 })
 
-describe("sleight of hand", () => {
-  test("replace the first card with a 7", () => {
+describe('sleight of hand', () => {
+  test('replace the first card with a 7', () => {
     const stack = [1, 2, 3]
     const position = 0
     const replacement = 7
@@ -44,7 +44,7 @@ describe("sleight of hand", () => {
     expect(setItem(stack, position, replacement)).toStrictEqual(expected)
   })
 
-  test("replace the middle card with a 5", () => {
+  test('replace the middle card with a 5', () => {
     const stack = [2, 2, 2]
     const position = 1
     const replacement = 5
@@ -53,7 +53,7 @@ describe("sleight of hand", () => {
     expect(setItem(stack, position, replacement)).toStrictEqual(expected)
   })
 
-  test("replace the last card with a 7", () => {
+  test('replace the last card with a 7', () => {
     const stack = [7, 7, 6]
     const position = 2
     const replacement = 7
@@ -63,8 +63,8 @@ describe("sleight of hand", () => {
   })
 })
 
-describe("make cards appear at the top", () => {
-  test("adding a second card at the top", () => {
+describe('make cards appear at the top', () => {
+  test('adding a second card at the top', () => {
     const stack = [1]
     const newCard = 5
 
@@ -72,7 +72,7 @@ describe("make cards appear at the top", () => {
     expect(insertItemAtTop(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a third card at the top", () => {
+  test('adding a third card at the top', () => {
     const stack = [1, 5]
     const newCard = 9
 
@@ -80,7 +80,7 @@ describe("make cards appear at the top", () => {
     expect(insertItemAtTop(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a fourth card at the top", () => {
+  test('adding a fourth card at the top', () => {
     const stack = [1, 5, 9]
     const newCard = 2
 
@@ -88,7 +88,7 @@ describe("make cards appear at the top", () => {
     expect(insertItemAtTop(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a different fourth card at the top", () => {
+  test('adding a different fourth card at the top', () => {
     const stack = [1, 5, 9]
     const newCard = 8
 
@@ -96,7 +96,7 @@ describe("make cards appear at the top", () => {
     expect(insertItemAtTop(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding multiple cards to the stack at the top", () => {
+  test('adding multiple cards to the stack at the top', () => {
     const stack = [1]
 
     insertItemAtTop(stack, 5)
@@ -107,8 +107,8 @@ describe("make cards appear at the top", () => {
   })
 })
 
-describe("make cards disappear", () => {
-  test("remove the card at the bottom", () => {
+describe('make cards disappear', () => {
+  test('remove the card at the bottom', () => {
     const stack = [1, 2, 3, 4]
     const position = 0
 
@@ -118,7 +118,7 @@ describe("make cards disappear", () => {
       // eslint-disable-next-line no-undef
       fail(
         new Error(
-          "The card has disappeared, but the stack has not changed in size. This magic trick has turned into actual magic. Perhaps a different method of removing the card will result in a stack that Elyse can work with..."
+          'The card has disappeared, but the stack has not changed in size. This magic trick has turned into actual magic. Perhaps a different method of removing the card will result in a stack that Elyse can work with...'
         )
       )
     }
@@ -126,7 +126,7 @@ describe("make cards disappear", () => {
     expect(removeItem(stack, position)).toStrictEqual(expected)
   })
 
-  test("remove the card at the top", () => {
+  test('remove the card at the top', () => {
     const stack = [1, 2, 3, 4]
     const position = 3
 
@@ -134,7 +134,7 @@ describe("make cards disappear", () => {
     expect(removeItem(stack, position)).toStrictEqual(expected)
   })
 
-  test("remove the second card", () => {
+  test('remove the second card', () => {
     const stack = [1, 2, 3, 4]
     const position = 1
 
@@ -142,7 +142,7 @@ describe("make cards disappear", () => {
     expect(removeItem(stack, position)).toStrictEqual(expected)
   })
 
-  test("remove the middle two cards", () => {
+  test('remove the middle two cards', () => {
     const stack = [1, 2, 3, 4]
 
     removeItem(stack, 1)
@@ -153,20 +153,20 @@ describe("make cards disappear", () => {
   })
 })
 
-describe("make the top card disappear", () => {
-  test("remove the only card from the top", () => {
+describe('make the top card disappear', () => {
+  test('remove the only card from the top', () => {
     const stack = [1]
     const expected = []
     expect(removeItemFromTop(stack)).toStrictEqual(expected)
   })
 
-  test("remove the card from the top", () => {
+  test('remove the card from the top', () => {
     const stack = [1, 2, 3]
     const expected = [1, 2]
     expect(removeItemFromTop(stack)).toStrictEqual(expected)
   })
 
-  test("remove two cards from the top", () => {
+  test('remove two cards from the top', () => {
     const stack = [1, 2, 3]
 
     removeItemFromTop(stack)
@@ -175,20 +175,22 @@ describe("make the top card disappear", () => {
     const expected = [1]
     expect(stack).toStrictEqual(expected)
   })
+})
 
-  test("remove the only card from the bottom", () => {
+describe('make the bottom card disappear', () => {
+  test('remove the only card from the bottom', () => {
     const stack = [1]
     const expected = []
     expect(removeItemAtBottom(stack)).toStrictEqual(expected)
   })
 
-  test("remove the card from the bottom", () => {
+  test('remove the card from the bottom', () => {
     const stack = [1, 2, 3]
     const expected = [2, 3]
     expect(removeItemAtBottom(stack)).toStrictEqual(expected)
   })
 
-  test("remove two cards from the bottom", () => {
+  test('remove two cards from the bottom', () => {
     const stack = [1, 2, 3]
 
     removeItemAtBottom(stack)
@@ -199,8 +201,8 @@ describe("make the top card disappear", () => {
   })
 })
 
-describe("make cards appear at the bottom", () => {
-  test("adding a second card to the bottom", () => {
+describe('make cards appear at the bottom', () => {
+  test('adding a second card to the bottom', () => {
     const stack = [1]
     const newCard = 5
 
@@ -208,7 +210,7 @@ describe("make cards appear at the bottom", () => {
     expect(insertItemAtBottom(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a third card to the bottom", () => {
+  test('adding a third card to the bottom', () => {
     const stack = [5, 1]
     const newCard = 9
 
@@ -216,7 +218,7 @@ describe("make cards appear at the bottom", () => {
     expect(insertItemAtBottom(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a fourth card to the bottom", () => {
+  test('adding a fourth card to the bottom', () => {
     const stack = [9, 5, 1]
     const newCard = 2
 
@@ -224,7 +226,7 @@ describe("make cards appear at the bottom", () => {
     expect(insertItemAtBottom(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding a different fourth card to the bottom", () => {
+  test('adding a different fourth card to the bottom', () => {
     const stack = [9, 5, 1]
     const newCard = 8
 
@@ -232,7 +234,7 @@ describe("make cards appear at the bottom", () => {
     expect(insertItemAtBottom(stack, newCard)).toStrictEqual(expected)
   })
 
-  test("adding multiple cards to the stack to the bottom", () => {
+  test('adding multiple cards to the stack to the bottom', () => {
     const stack = [1]
 
     insertItemAtBottom(stack, 5)
@@ -243,9 +245,9 @@ describe("make cards appear at the bottom", () => {
   })
 })
 
-describe("check your work", () => {
-  describe("an empty stack of cards", () => {
-    test("has 0 cards", () => {
+describe('check your work', () => {
+  describe('an empty stack of cards', () => {
+    test('has 0 cards', () => {
       const stack = []
 
       expect(checkSizeOfStack(stack, 0)).toBe(true)
@@ -253,8 +255,8 @@ describe("check your work", () => {
     })
   })
 
-  describe("a stack with a single card", () => {
-    test("has exactly 1 card", () => {
+  describe('a stack with a single card', () => {
+    test('has exactly 1 card', () => {
       const stack = [7]
 
       expect(checkSizeOfStack(stack, 0)).toBe(false)
@@ -263,8 +265,8 @@ describe("check your work", () => {
     })
   })
 
-  describe("a stack with the even cards", () => {
-    test("has exactly 4 cards", () => {
+  describe('a stack with the even cards', () => {
+    test('has exactly 4 cards', () => {
       const stack = [2, 4, 6, 8]
 
       expect(checkSizeOfStack(stack, 3)).toBe(false)
@@ -273,8 +275,8 @@ describe("check your work", () => {
     })
   })
 
-  describe("a stack with the odd cards", () => {
-    test("has exactly 5 cards", () => {
+  describe('a stack with the odd cards', () => {
+    test('has exactly 5 cards', () => {
       const stack = [1, 3, 5, 7, 9]
 
       expect(checkSizeOfStack(stack, 3)).toBe(false)
